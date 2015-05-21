8912268af1ff23abcf9667f8122e6d0fb2028457
michele ietri <michele.ietri@gmail.com>
2015-05-21 21:52:16 +0200

    datacleaning

--- Server/Fase3/datacleaning.py
+++ Server/Fase3/datacleaning.py
@@ -6,50 +6,26 @@ conn = psycopg2.connect(user='twitter', password='tsacs3m', dbname='dati', host=
 curs = conn.cursor()
 reader = conn.cursor()
 
-#states = {"sterreich","Belgi","Hrvatska","Kibris","Ceská Republika","Danmark","Eesti","Suomi","France","Deutschland","Ελλάδα","Magyarország","Ireland","Italia","Latvia","Lietuva","Luxemburg","Malta","Nederland","Polska","Portugal","România","Slovensko","Slovenija","España","Sverige","United Kingdom","България"}
 states = {"Österreich","België","Hrvatska","Kibris","Ceská Republika","Danmark","Eesti","Suomi","France","Deutschland","Ελλάδα","Magyarország","Ireland","Italia","Latvia","Lietuva","Luxemburg","Malta","Nederland","Polska","Portugal","România","Slovensko","Slovenija","España","Sverige","United Kingdom","България"}
 curs.execute('SELECT last_analized FROM last_id_analized')
 
+keywords = {"program", "code", "cod", "develop", "computer", "pc", "ict", "it", "language", "database", "db", "opensource", "apache", "tomcat", "api", "app", "css", "cloud", "site", "django", "cassandra", "dev", "arch", "linux", "ubuntu", "deb", "win", "csv", "json", "soft", "hard", "work", "job", "hire", "hiring", "hack", "ware", "digi", "xml", "css"}
+
 last_id = curs.fetchone()
+
+valid = False
 
 reader.execute('SELECT * FROM eutweets WHERE id > %s ORDER BY id',(last_id))
 
 for tuple in reader:
 	if tuple[4] in states:
 		if tuple[2] in tuple[1]:
-			print(tuple)
-			#sistemare elenco parole chiave
+			for word in keywords:
+				if word in tuple[1]
+					valid = True
+	if valid:
+		print(tuple)
 
 conn.commit()
 curs.close()
 reader.close()
-
-
-"Österreich"
-"België"
-"Hrvatska"
-"Kibris"
-"ceská republika"
-"Danmark"
-"Eesti"
-"Suomi"
-"France"
-"Deutschland"
-"Ελλάδα"
-"Magyarország"
-"Ireland"
-"Italia"
-"Latvia"
-"Lietuva"
-"Luxemburg"
-"Malta"
-"Nederland"
-"Polska"
-"Portugal"
-"România"
-"Slovensko"
-"Slovenija"
-"España"
-"Sverige"
-"United Kingdom"
-"България"
