d5e8be815ab2ed3f76384572d8d82cab696422d9
Michele Cicinelli <m.cicinelli1@gmail.com>
2015-05-29 11:37:29 +0200

    risolto bug accesso dati

---
+++ Server/Fase3/datafetching_v2.4.py
@@ -0,0 +1,86 @@
+import tweepy
+import psycopg2
+
+consumer = CONSUMER
+app = SECRET
+
+def get_auth(consumer_key, app_secret):
+	try:
+		auth = tweepy.AppAuthHandler(consumer_key, app_secret)
+	
+	  
+		api = tweepy.API(auth, wait_on_rate_limit=True,
+	                   wait_on_rate_limit_notify=True)
+		return api
+	except Exception as e:
+		raise tweepy.TweepError(e)
+
+
+
+def get_tweets(api):
+	try:	
+		conn = psycopg2.connect(user='', password='',
+	                            dbname='', host='',
+	                            port=5432)
+
+		curs = conn.cursor()
+
+		id_present = False
+
+		lang_list = "SELECT prog_lang, search_query FROM programming_languages"
+		curs.execute(lang_list)
+
+		languages = curs.fetchall()
+
+		langs ={}
+
+		for lang in languages:
+			langs[lang[0]] = lang[1]
+
+		count = 0
+
+		for key, value in langs.items():
+			
+			stringa = "SELECT id_str FROM last_id WHERE lang LIKE '" + key + "'"
+			
+
+			curs.execute(stringa)
+			last_id = curs.fetchone()
+			for status in tweepy.Cursor(api.search, q=value, count = 100, since_id = last_id[0]).items(1000):
+				
+				if id_present == False:
+					
+					curs.execute('UPDATE last_id SET id_str = %s WHERE lang LIKE %s',(status.id_str, key))
+					id_present = True
+
+				if status.place:
+					curs.execute('INSERT INTO eutweets (content, prog_lang, created_at, nation, id_str) VALUES (%s, %s, %s, %s, %s)', (status.text, key, status.created_at.date(), status.place.country, status.id_str))
+
+					count += 1
+
+			id_present = False
+			conn.commit()
+
+		print(str(count) + " tweets!")
+
+
+	except Exception as e:
+		raise tweepy.TweepError(e)
+	finally:
+		curs.close()
+		conn.close()
+
+
+
+
+
+if __name__ == '__main__':
+    import os
+    import sys
+
+    print("TwitterDB ver 2.4")
+    print("START")
+    auth = get_auth(consumer, app)
+    get_tweets(auth)
+    print("END")
+
